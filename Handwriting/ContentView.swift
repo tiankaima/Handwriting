3fc5ef4d9a0aeb404fa57de85a38f07a43a0a696
tiankaima <tiankaima@163.com>
2023-01-31 18:21:31 +0800

    Initial Commit

---
+++ Handwriting/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Handwriting
+//
+//  Created by Tiankai Ma on 2023/1/31.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Handwriting/HandwritingApp.swift
@@ -0,0 +1,17 @@
+//
+//  HandwritingApp.swift
+//  Handwriting
+//
+//  Created by Tiankai Ma on 2023/1/31.
+//
+
+import SwiftUI
+
+@main
+struct HandwritingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
